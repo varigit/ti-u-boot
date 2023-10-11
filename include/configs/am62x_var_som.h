d442efc3add3858c0c41b4cbe62b8f6b345972dd
Ken Sloat <kens@variscite.com>
2023-10-10 18:30:10 -0700

    am62x_var_som: Add support for Variscite VAR-SOM-AM62
    Add initial support for VAR-SOM-AM62. See https://www.variscite.com
    for more product information.
    
    Squashed History:
    
    e7ae721 am62x_var_som: configure AUDIO_EXT_REFCLK1 as output
    277c2dd am62x_var_som: symphony: disable mac_efuse
    4ba40a8 am62_var_som: defconfig: enable CONFIG_ENV_OVERWRITE
    84a6ade am62x_var_som: fix audio_refclk1_ctrl_clkout_en prototype
    64a0e84 am62x_var_som: move environment offset to 0x2B00000
    979d054 configs: am62x_var_som: Add support for USB DFU Boot mode
    de651a1 am62x_var_som: avoid memory conflict with 512MB dram
    
    Co-authored-by: Francesco Ferraro <francesco.f@variscite.com>
    Co-authored-by: Nate Drude <nate.d@variscite.com>
    
    Signed-off-by: Ken Sloat <ken.s@variscite.com>
    (cherry picked from commit c5e63efadb0f1f3c2183143c4c75e5bb2aa51605)

---
+++ include/configs/am62x_var_som.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration header file for K3 AM625 SoC family
+ *
+ * Copyright (C) 2020-2022 Texas Instruments Incorporated - https://www.ti.com/
+ *	Suman Anna <s-anna@ti.com>
+ */
+
+#ifndef __CONFIG_AM62X_VAR_SOM_H
+#define __CONFIG_AM62X_VAR_SOM_H
+#include <linux/sizes.h>
+#include <config_distro_bootcmd.h>
+
+/* DDR Configuration */
+#define CFG_SYS_SDRAM_BASE1             0x880000000
+
+#define DEFAULT_SDRAM_SIZE              SZ_512M
+
+/* Now for the remaining common defines */
+#include <configs/ti_armv7_common.h>
+
+#endif /* __CONFIG_AM62X_VAR_SOM_H */
