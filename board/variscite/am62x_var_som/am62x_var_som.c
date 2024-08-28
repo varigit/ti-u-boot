varigit/ti-u-boot

commit 985fa3f0202fd7120fb6c690cbfa1b4e8ac7659a

board: variscite: am62x_var_som: Update simple-framebuffer node in device-tree
Update simple-framebuffer device-tree node by enumerating framebuffer
related information in existing simple-framebuffer node in Linux
device-tree file and enabling it.

While at it, ignore error return value for framebuffer related DT node
updates as a non-zero return value for ft_board_setup is treated as a fatal
error causing board reset.

In case there is no simple-framebuffer stub detected in Linux kernel
device-tree and video is still active, then update the device-tree to
reserve the framebuffer region for the active splash screen.

This helps preserve the splash screen till the display server takes over
after OS is booted.

In case the simplefb node update or the framebuffer reservation fails we
treat it as a non-fatal error and avoid returning error to parent
function as the non-zero return value of ft_board_setup is treated as
fatal error which leads to board reset

Based on 75fe309112ce61ec6b4a4dc3e8850f547a2407d3

Signed-off-by: Eran Matityahu <eran.m@variscite.com>

board/variscite/am62x_var_som/am62x_var_som.c
@@ -15,6 +15,7 @@
 #include <splash.h>
 #include <dm/uclass.h>
 #include <k3-ddrss.h>
+#include <fdt_simplefb.h>
 #include <fdt_support.h>
 #include <asm/arch/hardware.h>
 #include <asm/arch/sys_proto.h>
@@ -188,6 +189,26 @@ static int video_setup(void)
 
 	return 0;
 }
+
+#if defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	int ret = -1;
+
+	if (IS_ENABLED(CONFIG_VIDEO)) {
+		if (IS_ENABLED(CONFIG_FDT_SIMPLEFB))
+			ret = fdt_simplefb_enable_and_mem_rsv(blob);
+
+		/* If simplefb is not enabled and video is active, then at least reserve
+		 * the framebuffer region to preserve the splash screen while OS is booting
+		 */
+		if (ret && video_is_active())
+			fdt_add_fb_mem_rsv(blob);
+	}
+
+	return 0;
+}
+#endif
 
 void spl_board_init(void)
 {

commit 08b215b61a6eb8bf8cd824a348b0047a546e9c36

board: variscite: am62x_var_som: Fix check for video support
Replace IS_ENABLED(CONFIG_VIDEO) with CONFIG_IS_ENABLED(VIDEO),
to distinctly check the video support enabled in the SPL and
'U-Boot proper' builds.

Fixes: 985fa3f0202fd7120fb6c690cbfa1b4e8ac7659a

Signed-off-by: Natalia Kovalenko <natasha.k@variscite.com>

board/variscite/am62x_var_som/am62x_var_som.c
@@ -195,7 +195,7 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 {
 	int ret = -1;
 
-	if (IS_ENABLED(CONFIG_VIDEO)) {
+	if (CONFIG_IS_ENABLED(VIDEO)) {
 		if (IS_ENABLED(CONFIG_FDT_SIMPLEFB))
 			ret = fdt_simplefb_enable_and_mem_rsv(blob);
 