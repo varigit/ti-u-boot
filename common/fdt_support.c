beagleboard/u-boot

commit e34654f67059401280dd8fd696e48f03a49f9733

common: Move framebuffer reservation to separate helper
Create separate helper for just reserving framebuffer region without
creating or enabling simple-framebuffer node.

This is useful for scenarios where user want to preserve the bootloader
splash screen till OS boots up and display server gets started without
displaying anything else in between and thus not requiring
simple-framebuffer.

Signed-off-by: Devarsh Thakkar <devarsht@ti.com>

common/fdt_support.c
@@ -21,6 +21,9 @@
 #include <exports.h>
 #include <fdtdec.h>
 #include <version.h>
+#include <video.h>
+
+DECLARE_GLOBAL_DATA_PTR;
 
 /**
  * fdt_getprop_u32_default_node - Return a node's property or a default
@@ -1969,6 +1972,24 @@ int fdt_setup_simplefb_node(void *fdt, int node, u64 base_address, u32 width,
 
 	return 0;
 }
+
+#if IS_ENABLED(CONFIG_VIDEO)
+int fdt_add_fb_mem_rsv(void *blob)
+{
+	struct fdt_memory mem;
+
+	/* nothing to do when the frame buffer is not defined */
+	if (gd->video_bottom == gd->video_top)
+		return 0;
+
+	/* reserved with no-map tag the video buffer */
+	mem.start = gd->video_bottom;
+	mem.end = gd->video_top - 1;
+
+	return fdtdec_add_reserved_memory(blob, "framebuffer", &mem, NULL, 0, NULL,
+			FDTDEC_RESERVED_MEMORY_NO_MAP);
+}
+#endif
 
 /*
  * Update native-mode in display-timings from display environment variable.

common/fdt_simplefb.c
@@ -102,7 +102,6 @@ int fdt_simplefb_enable_existing_node(void *blob)
 #if IS_ENABLED(CONFIG_VIDEO)
 int fdt_simplefb_enable_and_mem_rsv(void *blob)
 {
-	struct fdt_memory mem;
 	int ret;
 
 	/* nothing to do when video is not active */
@@ -113,15 +112,6 @@ int fdt_simplefb_enable_and_mem_rsv(void *blob)
 	if (ret)
 		return ret;
 
-	/* nothing to do when the frame buffer is not defined */
-	if (gd->video_bottom == gd->video_top)
-		return 0;
-
-	/* reserved with no-map tag the video buffer */
-	mem.start = gd->video_bottom;
-	mem.end = gd->video_top - 1;
-
-	return fdtdec_add_reserved_memory(blob, "framebuffer", &mem, NULL, 0, NULL,
-					  FDTDEC_RESERVED_MEMORY_NO_MAP);
+	return fdt_add_fb_mem_rsv(blob);
 }
 #endif

include/fdt_support.h
@@ -395,6 +395,8 @@ int arch_fixup_memory_node(void *blob);
 int fdt_setup_simplefb_node(void *fdt, int node, u64 base_address, u32 width,
 			    u32 height, u32 stride, const char *format);
 
+int fdt_add_fb_mem_rsv(void *blob);
+
 int fdt_overlay_apply_verbose(void *fdt, void *fdto);
 
 int fdt_valid(struct fdt_header **blobp);

varigit/ti-u-boot

commit 5234a56c535929c7bbaac14c9d3e70f11d457f3e

fdt_support: Replace IS_ENABLED(CONFIG_VIDEO) with CONFIG_IS_ENABLED(VIDEO)
The global_data structure members video_top and video bottom are
added in global_data in condition if CONFIG_IS_ENABLED(VIDEO),
which in SPL BUILD checks if CONFIG_SPL_VIDEO is enabled.
But the function fdt_add_fb_mem_rsv is defined in condition
if IS_ENABLED(CONFIG_VIDEO), which does not check whether video
support is enabled in the SPL stage, so this leads to the compilation error:
fdt_support.c:1982:15: error: 'gd_t' {aka 'volatile struct global_data'} has no member named 'video_bottom'

Replace IS_ENABLED(CONFIG_VIDEO) with CONFIG_IS_ENABLED(VIDEO),
to use the condition that will distinctly control the video support
enabled in SPL and 'U-Boot proper' builds.

Fixes: e34654f67059401280dd8fd696e48f03a49f9733

Signed-off-by: Natalia Kovalenko <natasha.k@variscite.com>

common/fdt_support.c
@@ -1973,7 +1973,7 @@ int fdt_setup_simplefb_node(void *fdt, int node, u64 base_address, u32 width,
 	return 0;
 }
 
-#if IS_ENABLED(CONFIG_VIDEO)
+#if CONFIG_IS_ENABLED(VIDEO)
 int fdt_add_fb_mem_rsv(void *blob)
 {
 	struct fdt_memory mem;