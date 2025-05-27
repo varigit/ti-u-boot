67e04403914a343483175a312c9a2dd212eded0c
FrancescoFerraro <francesco.f@variscite.com>
2025-05-27 09:45:02 +0200

    variscite: common: add ddr initialization for Variscite TI-based SOMs
    The k3-ddr.c and k3-ddr.h are copied from board/ti/common folder.
    The functions dram_init and dram_init_banksize have been modified to get
    the ddr size and configuration from EEprom.
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

---
+++ board/variscite/common/k3-ddr.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2024, Texas Instruments Incorporated - https://www.ti.com/
+ * Copyright (C) 2025, Variscite Ltd. - https://www.variscite.com/
+ */
+
+#ifndef __K3_DDR_INIT_H
+#define __K3_DDR_INIT_H
+
+int dram_init(void);
+int dram_init_banksize(void);
+
+#endif /* __K3_DDR_INIT_H */
