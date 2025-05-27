3ac7d9278d97a7f57ca3cba99937c56e8bc0d934
FrancescoFerraro <francesco.f@variscite.com>
2025-05-27 09:45:02 +0200

    variscite: common: add ddr initialization for Variscite TI-based SOMs
    The k3-ddr.c and k3-ddr.h are copied from board/ti/common folder.
    The functions dram_init and dram_init_banksize have been modified to get
    the ddr size and configuration from EEprom.
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

---
+++ board/variscite/common/k3-ddr.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2024, Texas Instruments Incorporated - https://www.ti.com/
+ * Copyright (C) 2025, Variscite Ltd. - https://www.variscite.com/
+ */
+
+#include <fdt_support.h>
+#include <dm/uclass.h>
+#include <k3-ddrss.h>
+#include <spl.h>
+
+#include "k3-ddr.h"
+#include "am62x_eeprom.h"
+#include "am62x_dram.h"
+
+int dram_init(void)
+{
+	int ret;
+
+	read_eeprom_header();
+
+	ret = fdtdec_setup_mem_size_base_lowest();
+	if (ret)
+		printf("Error setting up mem size and base. %d\n", ret);
+	else
+		/* Override fdtdec_setup_mem_size_base_lowest with memory size from EEPROM */
+		ret = var_dram_init_mem_size_base();
+
+	return ret;
+}
+
+int dram_init_banksize(void)
+{
+	return var_dram_init_banksize();
+}
