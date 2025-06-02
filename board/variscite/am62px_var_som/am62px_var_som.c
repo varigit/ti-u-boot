dcb1994443f2eb2ef57f57ba924fb86d185104df
FrancescoFerraro <francesco.f@variscite.com>
2025-06-02 15:11:16 +0200

    am62px_var_som: add support for VAR-SOM-AM62P
    Checked out from ti-u-boot-2024.04_10.01.10.04_var01 at commit 2a6647d, as follows:
        git checkout ti-u-boot-2024.04_10.01.10.04_var01 -- arch/arm/dts/k3-am62p*var-som*.dts*
        mv arch/arm/dts/k3-am62p5-var-som.dtsi dts/upstream/src/arm64/ti/
        mv arch/arm/dts/k3-am62p5-var-som-symphony.dts dts/upstream/src/arm64/ti/
        git checkout ti-u-boot-2024.04_10.01.10.04_var01 -- board/variscite/am62px_var_som
        git checkout ti-u-boot-2024.04_10.01.10.04_var01 -- include/configs/am62px_var_som.h
    
    Add am62px-var-som to:
        arch/arm/dts/Makefile
        arch/arm/mach-k3/am62px/Kconfig
    
    Squashed history:
    2ab10c6 am62px_var_som: add enviroment support to set cma size
    f074d36 am62px_var_som: drop nand enviroment
    29465d1 am62px_var_som: add DDR support from EEprom
    f544b42 am62px_var_som: move read_eeprom_header function to common
    958f554 arm: dts: am62p_var_som: increase PHY reset deassert delay to 100ms
    936c4ca arm: dts: k3-am62p-lp4: update DDR configs
    
    Include Variscite's requirements to am62px_var_som_r5_defconfig and am62px_var_som_a53_defconfig
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>
    (cherry picked from commit 7e1daad9f4e02e83fdf78788e71fa900ce465809)

---
+++ board/variscite/am62px_var_som/am62px_var_som.c
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Board specific initialization for AM62Px platforms
+ *
+ * Copyright (C) 2023 Texas Instruments Incorporated - https://www.ti.com/
+ *
+ */
+
+#include <efi_loader.h>
+#include <asm/arch/hardware.h>
+#include <asm/io.h>
+#include <cpu_func.h>
+#include <dm/uclass.h>
+#include <env.h>
+#include <fdt_support.h>
+#include <fdt_simplefb.h>
+#include <spl.h>
+#include <splash.h>
+#include <video.h>
+#include <asm/arch/k3-ddr.h>
+
+#include "../common/am62x_eeprom.h"
+#include "../common/am62x_dram.h"
+#include "../common/am62x_eth.h"
+#ifdef CONFIG_BOARD_LATE_INIT
+#include "../common/am62x_mmc.h"
+#endif
+
+DECLARE_GLOBAL_DATA_PTR;
+
+struct efi_fw_image fw_images[] = {
+	{
+		.image_type_id = AM62PX_VAR_SOM_TIBOOT3_IMAGE_GUID,
+		.fw_name = u"AM62PX_VAR_SOM_TIBOOT3",
+		.image_index = 1,
+	},
+	{
+		.image_type_id = AM62PX_VAR_SOM_SPL_IMAGE_GUID,
+		.fw_name = u"AM62PX_VAR_SOM_SPL",
+		.image_index = 2,
+	},
+	{
+		.image_type_id = AM62PX_VAR_SOM_UBOOT_IMAGE_GUID,
+		.fw_name = u"AM62PX_VAR_SOM_UBOOT",
+		.image_index = 3,
+	}
+};
+
+struct efi_capsule_update_info update_info = {
+	.dfu_string = "sf 0:0=tiboot3.bin raw 0 80000;"
+	"tispl.bin raw 80000 200000;u-boot.img raw 280000 400000",
+	.num_images = ARRAY_SIZE(fw_images),
+	.images = fw_images,
+};
+
+#if IS_ENABLED(CONFIG_SET_DFU_ALT_INFO)
+void set_dfu_alt_info(char *interface, char *devstr)
+{
+	if (IS_ENABLED(CONFIG_EFI_HAVE_CAPSULE_SUPPORT))
+		env_set("dfu_alt_info", update_info.dfu_string);
+}
+#endif
+
+int board_init(void)
+{
+	return 0;
+}
+
+#if IS_ENABLED(CONFIG_BOARD_LATE_INIT)
+#define ENV_STR_SIZE 10
+
+void set_bootdevice_env(void) {
+	int * boot_device = (int *) VAR_SCRATCH_BOOT_DEVICE;
+	char env_str[ENV_STR_SIZE];
+
+	snprintf(env_str, ENV_STR_SIZE, "%d", *boot_device);
+	env_set("boot_dev", env_str);
+
+	switch(*boot_device) {
+	case BOOT_DEVICE_MMC2:
+		printf("Boot Device: SD\n");
+		env_set("boot_dev_name", "sd");
+		break;
+	case BOOT_DEVICE_MMC1:
+		printf("Boot Device: eMMC\n");
+		env_set("boot_dev_name", "emmc");
+		break;
+	default:
+		printf("Boot Device: Unknown\n");
+		env_set("boot_dev_name", "unknown");
+		break;
+	}
+}
+
+#define SDRAM_SIZE_STR_LEN 5
+
+/* configure AUDIO_EXT_REFCLK1 pin as an output*/
+static void audio_refclk1_ctrl_clkout_en(void) {
+	volatile uint32_t *audio_refclk1_ctrl_ptr = (volatile uint32_t *)0x001082E4;
+	uint32_t audio_refclk1_ctrl_val = *audio_refclk1_ctrl_ptr;
+	audio_refclk1_ctrl_val |= (1 << 15);
+	*audio_refclk1_ctrl_ptr = audio_refclk1_ctrl_val;
+}
+
+int board_late_init(void)
+{
+	struct var_eeprom *ep = VAR_EEPROM_DATA;
+	char sdram_size_str[SDRAM_SIZE_STR_LEN];
+
+	audio_refclk1_ctrl_clkout_en();
+
+	env_set("board_name", "VAR-SOM-AM62P");
+
+	read_eeprom_header();
+	var_eeprom_print_prod_info(ep);
+
+	set_bootdevice_env();
+
+	snprintf(sdram_size_str, SDRAM_SIZE_STR_LEN, "%d",
+			(int) (gd->ram_size / 1024 / 1024));
+	env_set("sdram_size", sdram_size_str);
+
+#ifdef CONFIG_ENV_IS_IN_MMC
+	board_late_mmc_env_init();
+#endif
+
+#ifdef CONFIG_TI_AM65_CPSW_NUSS
+	var_setup_mac(ep);
+#endif
+
+	return 0;
+}
+#endif
+
+#if defined(CONFIG_XPL_BUILD)
+void spl_perform_fixups(struct spl_image_info *spl_image)
+{
+	if (IS_ENABLED(CONFIG_K3_DDRSS)) {
+		if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
+			fixup_ddr_driver_for_ecc(spl_image);
+	} else {
+		fixup_memory_node(spl_image);
+	}
+}
+#endif
+
+#if IS_ENABLED(CONFIG_SPL_BOARD_INIT)
+void spl_board_init(void)
+{
+	u32 val;
+
+#ifndef CONFIG_CPU_V7R
+	/* Save boot_device for U-Boot */
+	int * boot_device = (int *) VAR_SCRATCH_BOOT_DEVICE;
+	*boot_device = spl_boot_device();
+#endif
+
+	/* We have 32k crystal, so lets enable it */
+	val = readl(MCU_CTRL_LFXOSC_CTRL);
+	val &= ~(MCU_CTRL_LFXOSC_32K_DISABLE_VAL);
+	writel(val, MCU_CTRL_LFXOSC_CTRL);
+	/* Add any TRIM needed for the crystal here.. */
+	/* Make sure to mux up to take the SoC 32k from the crystal */
+	writel(MCU_CTRL_DEVICE_CLKOUT_LFOSC_SELECT_VAL,
+	       MCU_CTRL_DEVICE_CLKOUT_32K_CTRL);
+
+	enable_caches();
+	if (IS_ENABLED(CONFIG_SPL_SPLASH_SCREEN) && IS_ENABLED(CONFIG_SPL_BMP))
+		splash_display();
+
+	/* Init DRAM size for R5/A53 SPL */
+	dram_init_banksize();
+}
+#endif
+
+#if defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	int ret = -1;
+
+	if (IS_ENABLED(CONFIG_FDT_SIMPLEFB))
+		ret = fdt_simplefb_enable_and_mem_rsv(blob);
+
+	/* If simplefb is not enabled and video is active, then at least reserve
+	 * the framebuffer region to preserve the splash screen while OS is booting
+	 */
+	if (IS_ENABLED(CONFIG_VIDEO) && IS_ENABLED(CONFIG_OF_LIBFDT)) {
+		if (ret && video_is_active())
+			return fdt_add_fb_mem_rsv(blob);
+	}
+
+	return 0;
+}
+#endif
